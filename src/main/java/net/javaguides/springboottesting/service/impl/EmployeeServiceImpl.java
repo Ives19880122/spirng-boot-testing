2148110e92b4df608ce42f43cfaca3e81f6fa6b6
Ives <jianxing.imo99g@gmail.com>
2022-09-24 14:56:30 +0800

    feat: 建立EmployeeService相關內容

---
+++ src/main/java/net/javaguides/springboottesting/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboottesting.service.impl;
+
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
+import net.javaguides.springboottesting.model.Employee;
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import net.javaguides.springboottesting.service.EmployeeService;
+
+import java.util.Optional;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    /**
+     * 使用建構子注入方式,相較於Autowired,有助於測試使用
+     */
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+
+        if(savedEmployee.isPresent()){
+            throw new ResourceNotFoundException("Employee already exist with given email:"+ employee.getEmail());
+        }
+
+        return employeeRepository.save(employee);
+    }
+}
