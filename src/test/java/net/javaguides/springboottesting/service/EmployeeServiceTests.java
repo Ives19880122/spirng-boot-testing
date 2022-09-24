616035ecfb4d5636fa4eed60c1b6274df33833da
Ives <jianxing.imo99g@gmail.com>
2022-09-24 15:42:43 +0800

    fix: Service拋出異常測試撰寫

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
@@ -1,18 +1,23 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
 import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.repository.EmployeeRepository;
 import static org.assertj.core.api.Assertions.*;
 
 import net.javaguides.springboottesting.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
@@ -56,5 +61,20 @@ public class EmployeeServiceTests {
         assertThat(savedEmployee).isNotNull();
     }
 
+    // Junit test for saveEmployee method
+    @DisplayName("Junit test for saveEmployee method which throws exception")
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenThrowsException(){
+        // given - precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
 
+        // when - action or the behavior that we are going test
+        Assertions.assertThrows(ResourceNotFoundException.class,()->{
+                employeeService.saveEmployee(employee);
+        });
+
+        // then - verify the output 前面已經拋出異常,所以接續的儲存方法不會執行
+        verify(employeeRepository,never()).save(any(Employee.class));
+    }
 }
